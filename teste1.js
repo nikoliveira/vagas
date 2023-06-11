dcf03a6d1ed6f96f9b3679f5ceec2445b1c93f1a
Vinicius Vasconcelos Ferreira <viniciusvasconcelos.ferreira@gmail.com>
2023-06-11 20:00:05 -0300

    feat(teste1): insere validação e retorno de status condicionado ao tipo de retorno

--- teste1.js
+++ teste1.js
@@ -1,21 +1,24 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res, next) => {
+    const name = req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
+    if (!name) {
+        return res.status(400).send('Não foi informado nenhum usuário.');
     }
 
+    const search_user = data.filter(user => user.name.includes(name));
+
+    if (search_user.length > 0) {
+        res.status(200).send(search_user);
+    } else {
+        res.status(404).send('Usuário informado não encontrado.');
+    }
 };
 
-const getUsers = ( req, res, next ) => {
-    
+
+const getUsers = (req, res, next) => {
     res.send(data);
-    
 };
 
 module.exports = {
