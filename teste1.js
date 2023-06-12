733b35e80356a747f45616bf3d92d05bb016731c
Gabriela Hermenegildo Júnior <gabriela.hermenegil@meta.com.br>
2023-06-12 09:38:01 -0300

    teste 1 refatorado

--- teste1.js
+++ teste1.js
@@ -4,11 +4,14 @@ const getUser = ( req, res, next ) => {
     
     var name =  req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+    // for(let i = 0; i < data.length;  i++) {
+    //     if(i.name == name) {
+    //         res.send(data[i]);
+    //     }
+    // }
+
+    var user = data.find(u => u.name === name);
+    res.send(user);
 
 };
 
