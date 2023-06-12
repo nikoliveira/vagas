9a267194a54773f76c2f5645d7dcd4fa6271968a
Rafael <rafaelbrandao1992@gmail.com>
2023-06-12 10:42:35 -0300

    refactor: teste1.js changing for to find and add status code

--- teste1.js
+++ teste1.js
@@ -1,24 +1,22 @@
-var data =  require("./fakeData");
+let data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res) => {
+  const name = req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+  const user = data.find((user) => user.name === name);
 
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("Usuario não encontrado");
+  }
 };
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
+const getUsers = (_req, res) => {
+  res.send(data);
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers,
 };
