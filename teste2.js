20fee75831796f2007034ef3b880afcce28e062f
eletroswing <ytalosilvabatalha@gmail.com>
2023-06-12 19:25:32 -0300

    (refactor)test2: fixing type error in job, destructuring the body

--- teste2.js
+++ teste2.js
@@ -1,17 +1,14 @@
 var data =  require("./fakeData");
 
 module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
-    }
+    const { name, job } = req.body;
 
-    data.push(newUser)
-    
-    res.send(newUser);
+    const newUser = {
+        name,
+        job,
+    };
 
+    data.push(newUser);
+
+    res.status(201).json(newUser);
 };
