b10dc9cae54199e29d6d0c2c350da2b260d6dff0
Marlon <devmarlonferreira@gmail.com>
2023-06-12 17:25:02 -0300

    refactor(vagas): ajustando teste5

--- teste5.js
+++ teste5.js
@@ -1,9 +1,19 @@
+const fakeData = require("./fakeData");
 
+module.exports = function (req, res) {
+  const id = req.query.id;
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+  if (!id) {
+    return res.send({ message: "Informe um id" });
+  }
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
+  const found = fakeData.find((data) => {
+    if (data?.id === parseInt(id)) {
+      return data;
+    }
+  });
 
+  if (found)
+    res.send("Usuário " + found.name + " foi lido " + found.view + " vezes.");
+  else return res.send({ message: "Id não encontrado" });
 };
