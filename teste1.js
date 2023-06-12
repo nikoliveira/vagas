83b43400fb5d0030fc87d993a386f3c3e4fe375f
Janio_Souza <janiosouz001@gmail.com>
2023-06-11 21:58:04 -0300

    feat: Refatora busca de usuario por nome em teste1.js

--- teste1.js
+++ teste1.js
@@ -2,13 +2,22 @@ var data =  require("./fakeData");
 
 const getUser = ( req, res, next ) => {
     
-    var name =  req.query.name;
-
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
+    // Estou utilizando const em vez de var
+    // para evitar vazamentos de escopo.
+    const name =  req.query.name; 
+    
+    // Buscando o usuario por nome na base fake.
+    const result = data.find((person) => {
+        if (person.name == name) {
+            return person
         }
+    });
+
+    // Caso não seja encontrado é exibida uma mensagem para o usuario.
+    if(!result) {
+        return res.status(404).json({ message: 'Pessoa não encontrada.' });
     }
+    return res.send(result)
 
 };
 
