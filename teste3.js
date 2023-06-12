86946ed33e71d89a8cf3ee7442af144ad6b1094e
Gabriela Hermenegildo Júnior <gabriela.hermenegil@meta.com.br>
2023-06-12 09:58:29 -0300

    teste 2 e 3 feitos

--- teste3.js
+++ teste3.js
@@ -1,14 +1,23 @@
 var data =  require("./fakeData");
 
-module.exports = function(req, res) {
+module.exports = function(req, res, next) {
   
     var name =  req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
+    // for(let i = 0; i < data.length;  i++) {
+    //     if(i.name == name) {
+    //         data[i] = null;
+    //     }
+    // }
+
+    var userDeleted = data.find(u => u.name === name);
+    var userDeletedIndex = data.indexOf(userDeleted);
+    
+    if (userDeleted < 0) {
+        throw new Error('User not found.');
+    };
+
+    data.splice(userDeletedIndex, 1);
 
     res.send("success");
 
