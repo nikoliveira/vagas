2bbab4c94a286d4f5c32892bba633d785bca9d2a
Marlon <devmarlonferreira@gmail.com>
2023-06-12 16:40:56 -0300

    feat(vagas): adicionando novo middleware de auth

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+const authMiddleware = require("./middleware");
+
 var teste1 = require("./teste1");
 var teste2 = require("./teste2");
 var teste3 = require("./teste3");
@@ -12,7 +14,7 @@ var teste5 = require("./teste5");
 app.set('view engine', 'jade');
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 app.use(bodyParser.json());                        
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,8 +33,8 @@ app.get('/', function(req, res){
 app.get("/user", teste1.getUser);
 app.get("/users", teste1.getUsers);
 app.post("/users", teste2)
-app.delete("/users", teste3)
-app.put("/users", teste4)
+app.delete("/users", authMiddleware, teste3)
+app.put("/users", authMiddleware, teste4)
 app.get("/users/access", teste5);
 
 
