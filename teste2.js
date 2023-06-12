b8a7747b0d61fa4161adee650d2f7bd0d825978b
kaiocafe <105814791+kaiocafe@users.noreply.github.com>
2023-06-12 16:18:36 -0300

    Update teste2.js

--- teste2.js
+++ teste2.js
@@ -1,17 +1,19 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
+// Função para criar um novo usuário
+module.exports = function(req, res) {
+    const name = req.body.name;
+    const job = req.body.job;
+
+    // Cria um novo objeto de usuário com os dados fornecidos
+    const newUser = {
         name: name,
         job: job,
-    }
+    };
 
-    data.push(newUser)
+    // Adiciona o novo usuário ao array de dados
+    data.push(newUser);
     
+    // Retorna o novo usuário como resposta
     res.send(newUser);
-
 };
