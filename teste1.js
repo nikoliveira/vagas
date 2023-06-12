baa68bc5475223a8f11f7b1ae5fb7e7b0c95835c
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-06-12 15:33:59 -0300

    teste1 feito + instalação lib node-dev

--- teste1.js
+++ teste1.js
@@ -2,14 +2,27 @@ var data =  require("./fakeData");
 
 const getUser = ( req, res, next ) => {
     
-    var name =  req.query.name;
+    var name = req.query.name
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
+    if(!name) {
+        res.send("Por favor insira um nome de usuário após a url, exemplo: ?name=João%20Oliveira")
     }
 
+    const result = data.find(i=> i.name.toLowerCase() === name.toLowerCase())
+
+    if(!result) {
+        res.send("Nenhum usuário encontrado com este nome")
+    }
+
+    res.send(result)
+
+    // código antigo:
+
+    // for(let i = 0; i < data.length;  i++) {
+    //     // if(i.name == name) {
+    //         res.send(data[i]);
+    //     // }
+    // }
 };
 
 const getUsers = ( req, res, next ) => {
