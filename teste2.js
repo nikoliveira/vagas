b77acb4119b9b09d671639169eda574f4585f5e0
Kássia <kassia_guterres@outlook.com>
2023-06-12 14:01:53 -0300

    Typo corrected. Teste2 refactored

--- teste2.js
+++ teste2.js
@@ -1,17 +1,21 @@
-var data =  require("./fakeData");
+let data = require("./fakeData");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
-    }
+module.exports = function async (req, res) {
+  const { name, job } = req.body;
 
-    data.push(newUser)
-    
-    res.send(newUser);
+  let newUser = {
+    name: name,
+    job: job,
+  };
 
+  if (!name || !job) {
+    throw console.error("Nome e cargo devem estar preenchidos");
+  }
+
+  try {
+    data.push(newUser);
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
