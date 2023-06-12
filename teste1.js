12293f0612dc5c22cf3acdaa8467856ed0cf7841
Janio_Souza <janiosouz001@gmail.com>
2023-06-11 22:07:05 -0300

    fix: try catch em getUsers arquivo test1.js

--- teste1.js
+++ teste1.js
@@ -1,33 +1,38 @@
-var data =  require("./fakeData");
+var data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    // Estou utilizando const em vez de var
-    // para evitar vazamentos de escopo.
-    const name =  req.query.name; 
-    
-    // Buscando o usuario por nome na base fake.
-    const result = data.find((person) => {
-        if (person.name == name) {
-            return person
-        }
-    });
+const getUser = (req, res, next) => {
 
-    // Caso não seja encontrado é exibida uma mensagem para o usuario.
-    if(!result) {
-        return res.status(404).json({ message: 'Pessoa não encontrada.' });
+  // Estou utilizando const para evitar vazamentos de escopo.
+  const name = req.query.name;
+
+  // Buscando o usuario por nome na base fake.
+  const result = data.find((person) => {
+    if (person.name == name) {
+      return person
     }
-    return res.send(result)
+  });
+
+  // Caso não seja encontrado é exibida uma mensagem para o usuario.
+  if (!result) {
+    return res.status(404).json({ message: 'Pessoa não encontrada.' });
+  }
+  return res.send(result)
 
 };
 
-const getUsers = ( req, res, next ) => {
-    
+const getUsers = (req, res, next) => {
+
+  // Com este bloco try catch caso aconteça algum erro na comunicação com o banco a aplicação
+  // não vai crashar.
+  try {
     res.send(data);
-    
+  } catch {
+    res.status(500).json({ message: 'Internal error' });
+  }
+
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers
 };
