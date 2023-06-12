70eb1e18e039e57284b564d50033b75a99f5be71
Kássia <kassia_guterres@outlook.com>
2023-06-12 13:25:47 -0300

    Instaled nodemon. Teste1 concluded.

--- teste1.js
+++ teste1.js
@@ -1,24 +1,47 @@
-var data =  require("./fakeData");
+let data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = async (req, res, next) => {
+  let name = req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+  if (!name) {
+    throw console.error("Not Found");
+  }
+
+  // o async e await não são realmente necessários nesse caso específico, onde temos uma banco mockado muito pequeno, mas optei por deixa-los para simular o acesso a um banco de dados externo, e pensando também na escalabilidade e manutenção do código. 
+  try {
+   await data.forEach(function (value) {
+      if (value.name === name) {
+       return res.status(200).send(value);
+      }
+    });
+   
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 
 };
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
+const getUsers = async (req, res, next) => {
+  
+  if(data.length === 0){
+     res.status(200).send("No result for this search!");
+  }
+  
+  try {
+    const result = await data;
+    res.status(200).send(result);
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+ 
+  if (data.length === 0) {
+    throw console.error("Not Found");
+  }
+
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers,
 };
