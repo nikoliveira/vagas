9f2610e79a8f453e53457c2681adeafe1d28e0a6
Theus <matheusth.dev@gmail.com>
2023-06-13 12:02:41 -0300

    feat: get all users route

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,15 +1,11 @@
 import { Router } from "express";
 
-import * as T1 from "../../tests/teste1.js";
-import * as T2 from "../../tests/teste2.js";
-import * as T3 from "../../tests/teste3.js";
-import * as T4 from "../../tests/teste4.js";
-import * as T5 from "../../tests/teste5.js";
+import { getAllUsersController } from "../../controllers/user/getAllUsers.controller.js";
 
 const router = Router();
 
 export const userRoutes = () => {
-  // router.get("", T1.getUsers);
+  router.get("", getAllUsersController);
 
   return router;
 };
