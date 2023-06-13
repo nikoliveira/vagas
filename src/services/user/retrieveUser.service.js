7e1ea15423bdfc75ee7a62df3705bb22c7ca3593
Theus <matheusth.dev@gmail.com>
2023-06-13 12:13:52 -0300

    feat: retrieve user service

--- src/services/user/retrieveUser.service.js
+++ src/services/user/retrieveUser.service.js
@@ -1,5 +1,12 @@
-import data from "../../database/fakeData";
+import data from "../../database/fakeData.js";
+import { AppError } from "../../errors/appError.js";
 
 export const retrieveUserService = (userId) => {
-  const foundUser = data.find((user) => user.id === userId);
+  const userFound = data.find((user) => user.id === userId);
+
+  if (!userFound) {
+    throw new AppError(404, "user not found");
+  }
+
+  return userFound;
 };
