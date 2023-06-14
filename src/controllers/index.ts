c68483036f3db69b276e0da98d731a64d66f2a77
kennedybm <kennedy_muniz@hotmail.com>
2023-06-14 20:08:47 -0300

    feat: created to be the main file for all controller export files.

---
+++ src/controllers/index.ts
@@ -0,0 +1,13 @@
+import { createUserController } from "./user/createUser.controller";
+import { retrieveUserController } from "./user/retrieveUser.controller";
+import { listUsersController } from "./user/listUsers.controller";
+import { deleteUserController } from "./user/deleteUser.controller";
+import { updateUserController } from "./user/updateUser.controller";
+
+export {
+  createUserController,
+  retrieveUserController,
+  listUsersController,
+  deleteUserController,
+  updateUserController,
+};
