d68a80b47f0e04b6bc44a3c19f62afc7f3064c16
dev-luizf <luizfurtadomkt@hotmail.com>
2023-06-13 23:04:27 -0300

    feat: implementar funções do jwt

---
+++ src/utils/jwtHelper.ts
@@ -0,0 +1,28 @@
+import jwt, { SignOptions, JwtPayload } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import APIError from './errorClass';
+
+dotenv.config();
+
+const { JWT_SECRET } = process.env;
+
+if (!JWT_SECRET) {
+  throw new APIError('Um JWT_SECRET deve ser definido.', 'serverError');
+}
+
+const sign = (payload: JwtPayload) => {
+  const options: SignOptions = {
+    algorithm: 'HS256',
+    expiresIn: '1h',
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, options);
+  return token;
+};
+
+const verify = (token: string) => jwt.verify(token, JWT_SECRET, { algorithms: ['HS256'] });
+
+export {
+  sign,
+  verify,
+};
