69630506a047fac22bafe5e48392df1653e3efbd
kennedybm <kennedy_muniz@hotmail.com>
2023-06-14 20:05:45 -0300

    feat: created to be the main exports for the service files.

---
+++ src/services/index.ts
@@ -0,0 +1,15 @@
+import { createUserService } from "./user/createUser.service";
+import { retrieveUserService } from "./user/retrieveUser.service";
+import { listUsersService } from "./user/listUsers.service";
+import { createSessionService } from "./session/sessions.service";
+import { deleteUserService } from "./user/deleteUser.service";
+import { updateUserService } from "./user/updateUser.service";
+
+export {
+  createUserService,
+  retrieveUserService,
+  listUsersService,
+  createSessionService,
+  deleteUserService,
+  updateUserService,
+};
