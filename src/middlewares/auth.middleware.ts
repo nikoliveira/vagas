abcf1e61ca54aa0499604fdb33123453d113a3e8
dev-luizf <luizfurtadomkt@hotmail.com>
2023-06-13 23:06:37 -0300

    feat: implementa middleware de autorização e autenticação

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,41 @@
+import { RequestHandler, Request } from 'express';
+import { verify } from '../utils/jwtHelper';
+
+interface TokenPayload {
+    id: number,
+    isAdmin?: boolean,
+}
+
+interface RequestAuth extends Request {
+  user?: TokenPayload,
+}
+
+const tokenChecker: RequestHandler = (req: RequestAuth, res, next) => {
+  const token: string | undefined = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ error: 'Token não encontrado.' });
+  }
+
+  try {
+    const data = verify(token) as TokenPayload;
+    req.user = { ...data };
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ error: 'Token inválido.' });
+  }
+
+  next();
+};
+
+const permissionChecker: RequestHandler = (req: RequestAuth, res, next) => {
+    if (!req.user?.isAdmin) {
+        return res.status(403).json({ error: 'Acesso negado.' });
+    }
+    
+    next();
+}
+
+export {
+    tokenChecker,
+    permissionChecker,
+};
