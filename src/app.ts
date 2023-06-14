4bc948564300f75fc5195dfadc701106c8f43ec5
kennedybm <kennedy_muniz@hotmail.com>
2023-06-14 20:01:44 -0300

    feat: add Routes and Error handling. Configure the app to use Express with JSON parsing middleware. Add user routes and session route for handling user and session-related requests, respectively. Implement the handleAppErrorMiddleware to handle application-level errors and provide appropriate error responses.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,20 @@ import express from "express";
 import "express-async-errors";
 import "reflect-metadata";
 import userRoutes from "./routes/user.routes";
+import sessionRoutes from "./routes/session.route";
+import handleAppErrorMiddleware from "./errors/handleAppError";
 
 const app = express();
 
 app.use(express.json());
 
 //users routes
-app.use(userRoutes);
+app.use("/user", userRoutes);
+
+//session route
+app.use("/login", sessionRoutes);
+
+//errors
+app.use(handleAppErrorMiddleware);
 
 export default app;
