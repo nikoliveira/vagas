921641d81a43d5817c1e128c05572648a176c912
Theus <matheusth.dev@gmail.com>
2023-06-13 21:05:56 -0300

    refactor: read all users route

--- src/services/user/getAllUsers.service.js
+++ src/services/user/getAllUsers.service.js
@@ -3,7 +3,7 @@ import { data } from "../../database/fakeData.js";
 export const getAllUsersService = () => {
   const response = {
     count: data.length,
-    results: data.reverse(),
+    results: data.reverse().map((user) => user.info()),
   };
 
   return response;
