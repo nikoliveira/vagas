045c483856e48ade3248d3f47c96ae310faa92eb
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-06-15 01:34:46 -0300

    feat: uses dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
