191a6075b0c33d2f213bb8a14648c72a7d805642
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-06-15 02:02:24 -0300

    feat: fakeData with types and new format

---
+++ src/data/fakeData.ts
@@ -0,0 +1,13 @@
+import type UserI from '../interfaces/IUser';
+
+const data: UserI[] = [
+  {
+    id: 1,
+    name: 'Joao',
+    job: 'Desenvolvedor',
+    called: 0,
+    role: 'adm',
+  },
+];
+
+export default data;
