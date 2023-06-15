d94d796d874161acb424a4b5c660e6c0006f09df
kennedybm <kennedy_muniz@hotmail.com>
2023-06-15 10:05:54 -0300

    feat: Update the logic to increment the reading counter for a specific ID. The code checks if there is a reading counter entry with the provided ID in the  array. If not found, a new counter entry is created and added to the array. Then, the counter value is incremented by 1 for the existing counter entry. This change ensures accurate tracking of the number of readings for each ID.

--- src/services/user/retrieveUser.service.ts
+++ src/services/user/retrieveUser.service.ts
@@ -1,12 +1,22 @@
 import { AppError } from "../../errors/appError";
-import fakeData from "../../database/fakeData";
+import fakeData, { readingsCounter } from "../../database/fakeData";
 
 export const retrieveUserService = (id: string) => {
   const foundUser = fakeData.find((user) => user.id === id);
 
+  const foundReadings = readingsCounter.find((read) => read.id === id);
+
   if (!foundUser) {
     throw new AppError(404, "User not found!");
+  } else if (!foundReadings) {
+    let counterData = {
+      id: foundUser.id,
+      counter: 1,
+    };
+    readingsCounter.push(counterData);
   }
+
+  foundReadings!.counter += 1;
 
   return foundUser;
 };
