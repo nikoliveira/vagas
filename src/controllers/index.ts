7d97ce786fd1719a1abd789464bf7c990337012c
kennedybm <kennedy_muniz@hotmail.com>
2023-06-15 10:03:45 -0300

    feat: add readingsCounterController funcition in the main exports.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,6 +3,7 @@ import { retrieveUserController } from "./user/retrieveUser.controller";
 import { listUsersController } from "./user/listUsers.controller";
 import { deleteUserController } from "./user/deleteUser.controller";
 import { updateUserController } from "./user/updateUser.controller";
+import { readingsCounterController } from "./user/readingsCounter.controller";
 
 export {
   createUserController,
@@ -10,4 +11,5 @@ export {
   listUsersController,
   deleteUserController,
   updateUserController,
+  readingsCounterController,
 };
