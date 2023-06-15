24749e5123eaa7f6e6f648492d0dc05ded5c8dda
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-06-15 02:22:15 -0300

    feat: APP POO

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,61 @@
-import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import express, {type Application} from 'express';
 import bodyParser from 'body-parser';
-const app = express();
 
 import teste1 from './controllers/teste1';
 import teste2 from './controllers/teste2';
 import teste3 from './controllers/teste3';
 import teste4 from './controllers/teste4';
 import teste5 from './controllers/teste5';
+import UserController from './controllers/teste1';
 
-app.set('view engine', 'jade');
+class App {
+  protected app: Application;
+  protected userController: UserController;
+  protected port: string | 3000;
 
-app.use(express.json());
-app.use(express.urlencoded());
+  constructor() {
+    this.app = express();
+    this.userController = new UserController();
+    this.setConfig();
+    this.routes();
+    this.port = process.env.PORT ?? 3000;
+  }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+  setConfig() {
+    this.app.set('view engine', 'jade');
+    this.app.use(express.urlencoded());
 
-app.use(express.static(__dirname + '/public'));
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({extended: true}));
+  }
 
-// app.get('/', (req, res) => {
-//   res.send(`get user/ </br>
-//   get users/ </br>
-//   post users/ </br>
-//   delete users/ </br>
-//   put users/ </br>
-//   `);
-// });
+  routes() {
+    this.app.get('/user', this.userController.getUser);
+    this.app.get('/users', this.userController.getUsers);
+    // this.app.post('/users', this.userController);
+    // this.app.delete('/users', this.userController);
+    // this.app.put('/users', this.userController);
+    // this.app.get('/users/access', this.userController);
+  }
 
-app.get('/user', teste1.getUser);
-app.get('/users', teste1.getUsers);
-// app.post('/users', teste2);
-// app.delete('/users', teste3);
-// app.put('/users', teste4);
-// app.get('/users/access', teste5);
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Express server listening on port ${this.port}`);
+    });
+  }
+}
 
-export default app;
+// app.use(express.static(__dirname + '/public'));
+
+// // app.get('/', (req, res) => {
+// //   res.send(`get user/ </br>
+// //   get users/ </br>
+// //   post users/ </br>
+// //   delete users/ </br>
+// //   put users/ </br>
+// //   `);
+// // });
+
+export default App;
