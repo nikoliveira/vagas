645866ca8dff8eea938a7945cb215ff7fa43a1a1
daniloramin <daniloramin00@hotmail.com>
2023-06-14 22:19:06 -0300

    finish challenges

--- fakeData.js
+++ fakeData.js
@@ -1,11 +1,11 @@
-
-const fakeData  =  [
-    {
-        id: 1,
-        name: "João Oliveira",
-        job: "Desenvolvedor"
-    }
-]
-
+const fakeData = [
+  {
+    id: Math.random().toString(36).slice(2), // gera um ID aleatório, muda a base numérica e recorta a parte fracionada.
+    name: "João Oliveira",
+    job: "Desenvolvedor",
+    searched: 0,
+    admin: true,
+  },
+];
 
 module.exports = fakeData;
--- teste3.js
+++ teste3.js
@@ -1,15 +1,14 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+module.exports = function (req, res) {
+  const id = req.query.id;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      data.splice(i, 1);
+      return res.status(200).send({ message: "Success" });
     }
+  }
 
-    res.send("success");
-
+  return res.status(404).send({ message: "User not found" });
 };
