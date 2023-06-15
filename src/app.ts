1c791d08ef399478816cecd24b8b6fba8e1b2049
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-06-15 14:57:51 -0300

    feat: update route

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ class App {
     this.app.get('/users', this.userController.getUsers);
     this.app.post('/users', this.userController.setUser);
     this.app.delete('/users', this.userController.deleteUser);
-    // this.app.put('/users', this.userController);
+    this.app.put('/users', this.userController.updateUser);
     // this.app.get('/users/access', this.userController);
   }
 
