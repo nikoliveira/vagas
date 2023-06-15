908ab9893415e6f580a3724b26f73f2ab7056760
Marcos-vvc <marcos-dido@hotmail.com>
2023-06-14 21:38:42 -0300

    finalizado

--- teste2.js
+++ teste2.js
@@ -2,21 +2,18 @@ const data = require("./fakeData");
 const jwt = require("jsonwebtoken");
 
 const createUser = (req, res) => {
-  const { id, name, job, permissions } = req.body;
+  const { id, name, job } = req.body;
 
   const newUser = {
     id: id,
     name: name,
     job: job,
-    permissions: permissions,
   };
 
   data.push(newUser);
 
   res.send(newUser);
 };
-
-//O código abaixo gerar o token de permissão de acordo com o role criado.
 
 const generateToken = (permissions = [], role) => {
   return jwt.sign({ permissions, role }, process.env.SECRET_KEY);
--- teste6.js
+++ teste6.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const validatePermissions = (req, res, next) => {
-  // Verifica se o token JWT é fornecido no cabeçalho da requisição
   const token = req.headers["authorization"];
   if (!token) {
     return res
@@ -10,16 +9,13 @@ const validatePermissions = (req, res, next) => {
   }
 
   try {
-    // Verifica a validade do token e decodifica o payload
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     const { permissions, role } = decoded;
 
-    // Verifica se o usuário é admin e possui a permissão de delete ou update
     if (
       role === "admin" &&
       (permissions.includes("delete") || permissions.includes("update"))
     ) {
-      // Permite que a requisição prossiga
       next();
     } else {
       return res.status(403).json({
