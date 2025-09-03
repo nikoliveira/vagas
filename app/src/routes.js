77bb8c4065c4a38a22b54cccadd113a9a532bec4
Marcio Carolino <marciocarolino@gmail.com>
2025-09-03 10:44:25 -0300

    feat: changing the name of controller to service

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,14 +1,14 @@
 const app = require("express");
 const router = app.Router();
 
-const teste = require('./controllers/teste/teste');
-const teste1 = require("./controllers/teste1/teste1");
-const teste2 = require("./controllers/teste2/teste2");
-const teste3 = require("./controllers/teste3/teste3");
-const teste4 = require("./controllers/teste4/teste4");
-const teste5 = require("./controllers/teste5/teste5");
+const teste = require("./services/teste/teste");
+const teste1 = require("./services/teste1/teste1");
+const teste2 = require("./services/teste2/teste2");
+const teste3 = require("./services/teste3/teste3");
+const teste4 = require("./services/teste4/teste4");
+const teste5 = require("./services/teste5/teste5");
 
-router.get('/',teste.getAllUser)
+router.get("/", teste.getAllUser);
 router.get("/user", teste1.getUser);
 router.get("/users", teste1.getUsers);
 router.post("/users", teste2);
